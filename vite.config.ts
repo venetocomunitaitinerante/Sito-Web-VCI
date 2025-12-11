72abfa4f6500ef69207b129f9ffbdebab70117fc
VCI <venetocomunitaitinerante@oltrevia.it>
2025-12-11 17:20:54 +0100

    fix: update vite base field

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // base: './', // Ensures assets are loaded correctly on GitHub Pages sub-paths
+  base: '/Sito-Web-VCI/', // Ensures assets are loaded correctly on GitHub Pages sub-paths
   define: {
     // This allows process.env.API_KEY to work in the client-side code
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
